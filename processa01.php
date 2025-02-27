97c13209a39fad4444d6dfb0d3be078345730aed
soph-astronauta <161590070+soph-astronauta@users.noreply.github.com>
2025-02-27 10:49:49 -0300

    Add files via upload

---
+++ processa01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome = $_GET['cxnome'];
+    $idade =  $_GET['cxidade'];
+
+    echo "Nome digitado: $nome <br>";
+    echo "Idade digitada: $idade anos de vida! <br>";
+    echo "Parabéns! Voce tem 16 anos."
+?>
