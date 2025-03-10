9cf2dd5a05c6fcf64149bd46c3d791a3ae8e719f
soph-astronauta <161590070+soph-astronauta@users.noreply.github.com>
2025-03-10 08:23:58 -0300

    Add files via upload

---
+++ processa02.php
@@ -0,0 +1,9 @@
+<?php
+    $login = $_POST['cxlogin'];
+    $senha = $_POST['cxsenha'];
+
+    echo "Senha digitada: $senha <br>";
+    echo "Login digitado: $login <br>";
+    
+
+?>
---
+++ processa03.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $email = $_POST['cxemail'];
+    $datadenascimento = $_POST['cxdata de nascimento'];
+
+    echo "nome digitado: $nome <br>";
+    echo "email digitado: $email <br>";
+    echo "data de nascimento digitada: $datadenascimento <br>";
+    
+
+?>
